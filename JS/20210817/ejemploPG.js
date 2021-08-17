11e2c1f3a2559df2769c1bdc3d7d1fb4ae35d5be
maopineda <ingmauriciopineda@gmail.com>
2021-08-17 11:51:26 -0500

    Variables y concatenación de strings en JS

---
+++ JS/20210817/ejemploPG.js
@@ -0,0 +1,21 @@
+let uno = 8; //uno = 8
+let dos = uno; //dos = 8. Le asigna el valor de la variable uno (8) a la variable dos
+uno = 10; //uno = 10
+
+console.log(dos);
+
+//Ejercicio 4
+/*
+let nombre = "Nadir";
+let edad = 42;
+let cumpleanos = "octubre 10";
+let ciudad = "Medellín";
+let ocupacion = "estudiante";
+let hobbie = "fútbol";
+*/
+
+let nombre = "Mauricio", edad = 40, cumpleanos = "octubre 28", ciudad = "Medellín", ocupacion = "estudiante", hobbie = "fútbol";
+
+console.log("Soy " + nombre + " tengo " + edad + " años de edad. Mi cumpleaños es en " + cumpleanos + ". Vivo en " + ciudad + ", actualmente soy " + ocupacion + " y me gusta jugar al " + hobbie + ".")
+
+
---
+++ JS/20210817/script.js
@@ -0,0 +1,28 @@
+console.log("Hola Nadir");
+console.log(5);
+
+//Declarar e inicializar x
+let x = 1;
+
+//Declarar y
+let y;
+
+//Inicializar y
+y = 5;
+
+//Declaramos suma
+let suma;
+
+suma = x + y;
+
+//Imprimir suma
+console.log(suma);
+
+//Concatenar string
+let nombre = "Nadir";
+let apellido = "Pineda";
+let nombreCompleto = nombre + apellido;
+console.log(nombreCompleto);
+
+console.log("Mi nombre es " + nombre + " " + apellido);
+
